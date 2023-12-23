312721124f79d10565374da4732f3789a2d48056
hye1ee <hyewon0809@kaist.ac.kr>
2023-12-24 00:53:06 +0900

    [feat] Implement text animation

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,11 +3,10 @@ import styled from "styled-components";
 const PageLayout = styled.div<{ main?: boolean }>`
   width: 100%;
   height: ${(props) => (props.main === true ? "calc(100% - 105px)" : "100%")};
-  background-color: yellow;
 
   position: relative;
   box-sizing: border-box;
-  padding: 0px 40px;
+  padding: 0px 50px 0px 60px;
 
   flex: 0 0 auto;
 `;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,96 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+
 import PageLayout from "@components/PageLayout";
 
 import LinkedinIcon from "@assets/linkedinIcon.png";
+import GithubIcon from "@assets/githubIcon.png";
+
+const AnimateText = ["", "Research", "Design", "Develop", "Build"];
+
+const animation = {
+  initial: {
+    y: "-30px",
+    opacity: 0,
+  },
+  animate: {
+    y: "0%",
+    opacity: 1,
+  },
+};
 
 const Main = () => {
+  const [animateText, setAnimateText] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setAnimateText((prev) => (prev + 1) % 5);
+    }, 2000);
+    setTimeout(() => clearInterval(timer), 8500);
+  }, []);
+
   return (
-    <PageLayout main={true} id="main-page" style={{ paddingBottom: "100px" }}>
+    <PageLayout main={true} id="main-page" style={{ paddingBottom: "90px" }}>
       <MainWrapper>
-        {" "}
-        Main
         <MainContentRight>
           <div style={{ fontFamily: "Hello-Ketta", fontSize: "70px" }}>
             {"Hello, My name is"}
           </div>
           <div style={{ fontSize: "96px" }}>{"HYEWON\nLEE"}</div>
+          <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+            <MainIcon
+              onClick={() => window.open("https://www.linkedin.com/in/hye1ee/")}
+              src={LinkedinIcon}
+            />
+            <MainIcon
+              onClick={() => window.open("https://github.com/hye1ee")}
+              src={GithubIcon}
+            />
+          </div>
         </MainContentRight>
         <MainContentRightBottom>
-          <div style={{ fontSize: "25px" }}>
+          <div style={{ fontSize: "23px" }}>
+            {"I'm a "}
+            <span style={{ fontFamily: "Gilroy-Medium" }}>
+              {"Frontend Developer "}
+            </span>
             {
-              "I’m a Frontend Developer and a designer\nwho loves human computer interactions. \n\nMajoring computer science, industrial design at KAIST 🇰🇷.\nCurrently based in NYC 🗽, studying at New York University."
+              "& Designer\nwho loves human computer interactions. \n\nMajoring computer science, industrial design at "
             }
+            <span
+              onClick={() =>
+                window.open("https://www.kaist.ac.kr/en/html/kaist/01.html")
+              }
+              style={{ textDecoration: "underline", cursor: "pointer" }}
+            >
+              {"KAIST 🇰🇷"}
+            </span>
+            {"\nCurrently based in NYC 🗽, studying at New York University."}
           </div>
         </MainContentRightBottom>
+        <MainContentLeftBottom style={{ fontSize: "96px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              height: "fit-content",
+              alignItems: "flex-end",
+            }}
+          >
+            {"I  "}
+            <MainMotionContainer
+              key={animateText}
+              variants={animation}
+              initial={"initial"}
+              animate={"animate"}
+              transition={{ type: "spring" }}
+            >
+              {AnimateText[animateText]}
+            </MainMotionContainer>
+          </div>
+          {"CREATIVE\nINTERACTIONS"}
+        </MainContentLeftBottom>
       </MainWrapper>
     </PageLayout>
   );
@@ -49,7 +118,7 @@ const MainContentRight = styled.div`
 const MainContentRightBottom = styled.div`
   position: absolute;
   right: 0;
-  bottom: 10px;
+  bottom: 0;
 
   display: flex;
   flex-direction: column;
@@ -57,4 +126,35 @@ const MainContentRightBottom = styled.div`
   justify-content: flex-start;
 
   text-align: right;
+`;
+
+const MainContentLeftBottom = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: -20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: flex-start;
+
+  text-align: left;
+`;
+
+const MainIcon = styled.img`
+  width: 42px;
+  height: 42px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 50%;
+  }
+  transition: all 0.3s;
+`;
+
+const MainMotionContainer = styled(motion.div)`
+  font-family: "Hello-Ketta";
+  font-size: 140px;
+
+  position: relative;
 `;
