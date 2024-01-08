615c97fe028b5548ef7c5faa67603fbb47e0c1b3
hye1ee <hyewon0809@kaist.ac.kr>
2024-01-08 22:53:30 +0900

    [feat] Implement lottie play logic

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import LottieHyewonResearch from "@assets/hyewon_research.json";
+import LottieHyewonDesign from "@assets/hyewon_design.json";
+import LottieHyewonDevelop from "@assets/hyewon_develop.json";
+
 import { motion } from "framer-motion";
 
 import PageLayout from "@components/PageLayout";
@@ -29,10 +34,31 @@ const Main = ({
   setAnimateEvent: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const [animateText, setAnimateText] = useState<number>(0);
+  const lottieResearchRef = useRef<LottieRefCurrentProps>(null);
+  const lottieDesignRef = useRef<LottieRefCurrentProps>(null);
+  const lottieDevelopRef = useRef<LottieRefCurrentProps>(null);
 
   useEffect(() => {
     setAnimateEvent(true);
   }, []);
+
+  useEffect(() => {
+    if (
+      animateEvent &&
+      lottieResearchRef.current &&
+      lottieDesignRef.current &&
+      lottieDevelopRef.current
+    ) {
+      if (animateText === 1) lottieResearchRef.current.play();
+      else if (animateText === 2) lottieDesignRef.current.play();
+      else if (animateText === 3) lottieDevelopRef.current.play();
+      else if (animateText === 0) {
+        lottieResearchRef.current.stop();
+        lottieDevelopRef.current.stop();
+        lottieDesignRef.current.stop();
+      }
+    }
+  }, [animateText]);
 
   useEffect(() => {
     if (animateEvent) {
@@ -50,9 +76,35 @@ const Main = ({
   return (
     <PageLayout main={true} id="main-page" style={{ paddingBottom: "90px" }}>
       <MainWrapper>
-        <img
-          style={{ width: "60%", position: "relative", top: "-25px" }}
-          src={TestImg}
+        <Lottie
+          lottieRef={lottieResearchRef}
+          style={{ width: "25%", position: "absolute", left: "0px" }}
+          autoplay={false}
+          loop={false}
+          animationData={LottieHyewonResearch}
+        />
+        <Lottie
+          lottieRef={lottieDesignRef}
+          style={{
+            width: "23%",
+            position: "absolute",
+            left: "42%",
+          }}
+          autoplay={false}
+          loop={false}
+          animationData={LottieHyewonDesign}
+        />
+        <Lottie
+          lottieRef={lottieDevelopRef}
+          style={{
+            width: "23%",
+            position: "absolute",
+            left: "20%",
+            top: "12%",
+          }}
+          autoplay={false}
+          loop={false}
+          animationData={LottieHyewonDevelop}
         />
         <MainContentRight>
           <div style={{ fontFamily: "Hello-Ketta", fontSize: "70px" }}>
