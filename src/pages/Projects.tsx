cdcd68187af0fa81c40dfd0ae930e2778f6dfebf
hye1ee <hyewon0809@kaist.ac.kr>
2024-03-15 19:11:19 -0400

    [feat] Add project description

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,8 +8,10 @@ import RadioneImg from "@assets/projects/radione.png";
 
 import ArtVentureImg from "@assets/projects/artventure.png";
 import RunThroughGravityImg from "@assets/projects/runthroughgravity.png";
-import HelloWorldImg from "@assets/projects/helloworld.png";
-import ReliefCubeImg from "@assets/projects/reliefcube.png";
+import BlackGhostImg from "@assets/projects/blackghost.png";
+
+// import HelloWorldImg from "@assets/projects/helloworld.png";
+// import ReliefCubeImg from "@assets/projects/reliefcube.png";
 
 import ContentLayout from "@components/ContentLayout";
 
@@ -23,7 +25,7 @@ const Projects = () => {
           year={2024}
           type="Web"
           tags={["AR", "Mirror Room"]}
-          description=""
+          description="Minionize is a web-based mirror room space where user can generate their minion mask and try it on in the mirror room via AR experience. It conveys a message that 'What is the matter even if we are different look like minions?'"
         />
         <ProjectItem
           title="MultiDreamer"
@@ -31,7 +33,8 @@ const Projects = () => {
           year={2023}
           type="ML"
           tags={["3D"]}
-          description=""
+          description="MultiDreamer is an end-to-end machine learning pipeline based on SyncDreamer. It enables to generate of 3D meshes from a single-view multi-objects image. The chamfer distance between the inferred model and the ground truth model has decreased by 60% compared with the baseline."
+          link="https://github.com/hye1ee/MultiDreamer"
         />
         <ProjectItem
           title="VIVID"
@@ -39,7 +42,8 @@ const Projects = () => {
           year={2023}
           type="Web"
           tags={["HCI", "LLM"]}
-          description=""
+          description="VIVID is an LLM(Large Language Model) collaborative authoring tool to design, evaluate, and modify pedagogical dialogues. It is built on React with Typescript and Redux, utilizing OpenAI APIs."
+          link="https://github.com/hye1ee/VIVID-demo"
         />
         <ProjectItem
           title="MySelfieRoom"
@@ -47,7 +51,8 @@ const Projects = () => {
           year={2023}
           type="Web"
           tags={["ML", "Social"]}
-          description=""
+          description="MySelfieRoom is a web-based online photobooth service that can take, edit, and share photos. Developed with Typescript and React. To handle the image processing procedure, states are managed by Recoil."
+          link="https://github.com/hye1ee/My-SelfieRoom"
         />
         <ProjectItem
           title="Radione"
@@ -55,7 +60,7 @@ const Projects = () => {
           year={2023}
           type="Product"
           tags={["UX", "Circuit"]}
-          description=""
+          description="Radione is a brand new type of radio that improved the usability of finding channels. It simplifies the process at one touch by providing a channel chip that contains channel information as resistors."
         />
         <ProjectItem
           title="Artventure"
@@ -63,7 +68,7 @@ const Projects = () => {
           year={2023}
           type="Arduino"
           tags={["UX", "Visual Thinking"]}
-          description=""
+          description="Artventure is an interactive art toy inspired by a pictionary game. It provides an opportunity to enjoy the visual thinking process through art, away from digital stimulations."
         />
         <ProjectItem
           title="RunThroughGravity"
@@ -71,9 +76,19 @@ const Projects = () => {
           year={2022}
           type="Web"
           tags={["P5js", "Firebase"]}
-          description=""
+          description="RunThroughGravity is a mini arcade game implemented with p5.js based on pure Javascript. It developed by adapting an observer pattern to organize game objects and utilized Firebase to manage users."
+          link="https://github.com/hye1ee/run-through-gravity"
         />
         <ProjectItem
+          title="BlackGhost"
+          src={BlackGhostImg}
+          year={2022}
+          type="Web"
+          tags={["Electron", "Automator"]}
+          description="BlackGhost is a web-based puzzle game built for Korean reality game show, Treasure Hunt. It is implemented based on React, packed as an app by Electron. To be utilized in a game, its auto-play and termination logic have been developed with a shell script using Automator to run in the background process."
+          link="https://github.com/hye1ee/blackghost"
+        />
+        {/* <ProjectItem
           title="HelloWorld"
           src={HelloWorldImg}
           year={2021}
@@ -88,7 +103,7 @@ const Projects = () => {
           type="Product"
           tags={["UX"]}
           description=""
-        />
+        /> */}
       </ProjectsContainer>
     </ContentLayout>
   );
@@ -112,6 +127,7 @@ interface ProjectItemProps {
   year: number;
   type: "Web" | "Arduino" | "ML" | "Product";
   tags?: string[];
+  link?: string;
   description: string;
 }
 const ProjectItem = (props: ProjectItemProps) => {
@@ -138,7 +154,11 @@ const ProjectItem = (props: ProjectItemProps) => {
 
         <div style={{ fontSize: "15px", color: "darkgray" }}>{props.year}</div>
       </ProjectTitleWrapper>
-      <ProjectItemImgWrapper>
+      <ProjectItemImgWrapper
+        onClick={() => {
+          if (props.link) window.open(props.link);
+        }}
+      >
         <ProjectOverlay>{props.description}</ProjectOverlay>
         <ProjectItemImg alt={props.title} src={props.src} />
       </ProjectItemImgWrapper>
@@ -188,15 +208,17 @@ const ProjectOverlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.6);
   transition: all 0.5s;
-
   box-sizing: border-box;
-  padding: 30px;
-  font-size: 18px;
+  padding: 35px;
+  font-size: 16px;
+  line-height: 1.8;
   color: white;
 
   opacity: 0;
+
+  cursor: pointer;
   &:hover {
     opacity: 1;
   }
@@ -212,7 +234,6 @@ const ProjectItemImgWrapper = styled.div`
   border: 1px solid lightgray;
   overflow: hidden;
   position: relative;
-  cursor: pointer;
 `;
 
 const ProjectItemImg = styled.img`
